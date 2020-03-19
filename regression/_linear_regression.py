9262a20f4925b257d0bdb9359e0f15595e3bcbfb
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2020-03-19 22:46:04 +0200

    Move gradient descent to a separate package

--- regression/_linear_regression.py
+++ regression/_linear_regression.py
@@ -1,4 +1,5 @@
 import numpy as np
+from ..optimization import gradient_descent
 
 
 class LinearRegression:
@@ -6,7 +7,7 @@ class LinearRegression:
 
     :param regularization_param: L2 regularization parameter (must be >= 0, when set exactly to 0 no regression is used)
     :param use_gradient_descent: Whether to use gradient descent instead of normal equation to fit the model
-    :param learning_rate: An initial learning rate of gradient descent (used only if use_gradient_descent set to True, can be automatically reduced if too high)
+    :param learning_rate: Initial learning rate of gradient descent (used only if use_gradient_descent set to True, can be automatically reduced if too high)
     :param accuracy: Accuracy of gradient descent (used only if use_gradient_descent set to True)
     :param max_iterations: Maximum iterations count of gradient descent (used only if use_gradient_descent set to True)
     """
@@ -30,9 +31,10 @@ class LinearRegression:
         X = LinearRegression._add_intercept(X)
 
         if self._use_gradient_descent:
-            self._gradient_descent(X, y)
+            self._coefs = gradient_descent(self._cost, self._cost_gradient, np.zeros(X.shape[1]), (X, y),
+                                           self._learning_rate, self._accuracy, self._max_iterations)
         else:
-            self._normal_equation(X, y)
+            self._coefs = self._normal_equation(X, y)
 
     def predict(self, X):
         """Predict target values.
@@ -42,39 +44,11 @@ class LinearRegression:
         """
         return LinearRegression._linear_func(LinearRegression._add_intercept(X), self._coefs)
 
-    def _gradient_descent(self, X, y):
-        coefs = np.zeros(X.shape[1])
-        cost = self._cost(X, y, coefs)
-        rate = self._learning_rate
-
-        for iteration in range(self._max_iterations):
-            previous_coefs = coefs
-            previous_cost = cost
-
-            gradient = self._cost_gradient(X, y, coefs)
-
-            # If absolute values of all partial derivatives are small enough, than coefficients are close enough to the
-            # minimum and there is no need to continue gradient descent
-            if np.sum(np.abs(gradient) > self._accuracy) == 0:
-                break
-
-            coefs = coefs - rate * gradient
-
-            # If new coefficients made the cost higher, they are reverted to previous values and learning rate is
-            # decreased
-            cost = self._cost(X, y, coefs)
-            if cost > previous_cost:
-                coefs = previous_coefs
-                cost = previous_cost
-                rate /= 2
-
-        self._coefs = coefs
-
-    def _cost(self, X, y, coefs):
+    def _cost(self, coefs, X, y):
         return (((LinearRegression._linear_func(X, coefs) - y)**2).sum() +
                 self._regularization_param*(coefs[1:]**2).sum()) / X.shape[0]
 
-    def _cost_gradient(self, X, y, coefs):
+    def _cost_gradient(self, coefs, X, y):
         # Intercept should not be regularized, so it is set to 0 in a copy of the coefficients vector
         coefs_without_intercept = coefs.copy()
         coefs_without_intercept[0] = 0
@@ -85,7 +59,7 @@ class LinearRegression:
         features_count = X.T.shape[0]
         regularization_matrix = self._regularization_param*np.identity(features_count)
         regularization_matrix[0, 0] = 0
-        self._coefs = np.linalg.pinv(X.T @ X + regularization_matrix) @ X.T @ y
+        return np.linalg.pinv(X.T @ X + regularization_matrix) @ X.T @ y
 
     @staticmethod
     def _linear_func(X, coefs):
