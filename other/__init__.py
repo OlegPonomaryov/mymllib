6da8550ecc1e3f8be326223f0179d3c7215260d1
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2020-04-05 06:52:24 +0300

    Change 'other' package name to 'tools'

--- other/__init__.py
+++
@@ -1,2 +0,0 @@
-"""A package with useful stuff for developing and testing the library."""
-from ._gradient import gradient
---
+++ tools/__init__.py
@@ -0,0 +1,2 @@
+"""A package with different tools that are used by the library or may be useful for testing and using it."""
+from ._gradient import gradient
