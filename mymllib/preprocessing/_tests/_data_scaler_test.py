7566eed8103a9f2b9f0805074553eff91512f2ad
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2020-04-09 20:32:50 +0300

    Use functions from numpy.testing instead of ordinary NumPy functions in test modules

--- mymllib/preprocessing/_tests/_data_scaler_test.py
+++ mymllib/preprocessing/_tests/_data_scaler_test.py
@@ -1,5 +1,5 @@
 """Tests for the '_data_scaler' module."""
-import numpy as np
+from numpy.testing import assert_array_equal
 from mymllib.preprocessing import DataScaler
 
 
@@ -15,4 +15,4 @@ def test_fit_scale__fit_on_one_matrix_and_scale_another__correctly_scaled_matrix
     data_scaler.fit(A)
     C = data_scaler.scale(B)
 
-    assert np.array_equal(C, B_scaled)
+    assert_array_equal(C, B_scaled)
--- mymllib/preprocessing/_tests/_functions_test.py
+++ mymllib/preprocessing/_tests/_functions_test.py
@@ -1,5 +1,6 @@
 """Tests for the '_functions' module."""
-import numpy as np
+from numpy import ndarray
+from numpy.testing import assert_array_equal
 from mymllib.preprocessing import to_numpy, add_intercept, add_polynomial
 import pytest
 
@@ -9,7 +10,7 @@ def test_to_numypy__list_passed__ndarray_returned():
 
     b = to_numpy(a)
 
-    assert isinstance(b, np.ndarray)
+    assert isinstance(b, ndarray)
 
 
 def test_add_intercept__matrix_passed__matrix_with_intercept_returned():
@@ -22,12 +23,12 @@ def test_add_intercept__matrix_passed__matrix_with_intercept_returned():
 
     B = add_intercept(A)
 
-    assert np.array_equal(B, A_intercept)
+    assert_array_equal(B, A_intercept)
 
 
 def test_add_polynomial__matrix_and_floating_point_degree_passed__value_error_raised():
-    A = np.asarray([[1, 2, 3],
-                    [1, 2, 3]])
+    A = [[1, 2, 3],
+         [1, 2, 3]]
     polynomial_degree = 1.5
 
     with pytest.raises(ValueError):
@@ -35,8 +36,8 @@ def test_add_polynomial__matrix_and_floating_point_degree_passed__value_error_ra
 
 
 def test_add_polynomial__matrix_and_negative_degree_passed__value_error_raised():
-    A = np.asarray([[1, 2, 3],
-                    [1, 2, 3]])
+    A = [[1, 2, 3],
+         [1, 2, 3]]
     polynomial_degree = -1
 
     with pytest.raises(ValueError):
@@ -44,8 +45,8 @@ def test_add_polynomial__matrix_and_negative_degree_passed__value_error_raised()
 
 
 def test_add_polynomial__matrix_and_degree_0_passed__value_error_raised():
-    A = np.asarray([[1, 2, 3],
-                    [1, 2, 3]])
+    A = [[1, 2, 3],
+         [1, 2, 3]]
     polynomial_degree = 0
 
     with pytest.raises(ValueError):
@@ -53,37 +54,37 @@ def test_add_polynomial__matrix_and_degree_0_passed__value_error_raised():
 
 
 def test_add_polynomial__matrix_and_degree_1_passed__same_matrix_returned():
-    A = np.asarray([[1, 2, 3],
-                    [1, 2, 3]])
+    A = [[1, 2, 3],
+         [1, 2, 3]]
     polynomial_degree = 1
 
     B = add_polynomial(A, polynomial_degree)
 
-    assert np.array_equal(B, A)
+    assert_array_equal(B, A)
 
 
 def test_add_polynomial__matrix_and_degree_2_passed__up_to_2nd_degree_polynomial_matrix_returned():
-    A = np.asarray([[1, 2, 3],
-                    [1, 2, 3]])
-    A_poly = np.asarray([[1, 2, 3, 1, 2, 3, 4, 6, 9],
-                         [1, 2, 3, 1, 2, 3, 4, 6, 9]])
+    A = [[1, 2, 3],
+         [1, 2, 3]]
+    A_poly = [[1, 2, 3, 1, 2, 3, 4, 6, 9],
+              [1, 2, 3, 1, 2, 3, 4, 6, 9]]
     polynomial_degree = 2
 
     B = add_polynomial(A, polynomial_degree)
 
-    assert np.array_equal(B, A_poly)
+    assert_array_equal(B, A_poly)
 
 
 def test_add_polynomial__matrix_and_degree_4_passed__up_to_4th_degree_polynomial_matrix_returned():
-    A = np.asarray([[4, 6, 3],
-                    [4, 6, 3]])
+    A = [[4, 6, 3],
+         [4, 6, 3]]
     polynomial_degree = 4
-    A_poly = np.asarray(
+    A_poly =\
         [[4, 6, 3, 16, 24, 12, 36, 18, 9, 64, 96, 48, 144, 72, 36, 216, 108, 54, 27, 256, 384, 192, 576, 288, 144, 864,
           432, 216, 108, 1296, 648, 324, 162, 81],
          [4, 6, 3, 16, 24, 12, 36, 18, 9, 64, 96, 48, 144, 72, 36, 216, 108, 54, 27, 256, 384, 192, 576, 288, 144, 864,
-          432, 216, 108, 1296, 648, 324, 162, 81]])
+          432, 216, 108, 1296, 648, 324, 162, 81]]
 
     B = add_polynomial(A, polynomial_degree)
 
-    assert np.array_equal(B, A_poly)
+    assert_array_equal(B, A_poly)
