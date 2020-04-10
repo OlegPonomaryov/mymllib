389f1157a529aaf3fb48e194b42dc7ba0fd8452d
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2020-04-10 15:24:06 +0300

    Tests for cost functions gradients of regressions

--- mymllib/classification/_tests/_logistic_regression_test.py
+++ mymllib/classification/_tests/_logistic_regression_test.py
@@ -1,6 +1,10 @@
 """Tests for the '_logistic_regression' module."""
-from numpy.testing import assert_array_equal
+import numpy as np
+from numpy.testing import assert_array_equal, assert_allclose
 from mymllib.classification import LogisticRegression
+from mymllib.preprocessing import DataScaler, to_numpy
+from mymllib.tools import gradient
+
 
 # A simple dataset for tests
 # Classes:
@@ -20,8 +24,26 @@ X = [[24, 32],
      [22, 3]]
 y = [0, 1, 2,  0, 1, 2,  0, 1, 2]
 
-# A binary version of the y in which class 1 is class 2 from the multiclass version and class 0 stands for all other
-# classes
+# Due to significant digits limitation of floating-point variables an output of the logistic function for very large or
+# very small arguments is rounded, so altering such an argument a little bit won't change the result of the function,
+# making numerical gradient calculation impossible. This can be avoided by scaling X and therefore decreasing absolute
+# values of its elements.
+X_scaled = DataScaler().fit(X).scale(X)
+
+# A one-hot version of the y
+y_one_hot = [[1, 0, 0],
+             [0, 1, 0],
+             [0, 0, 1],
+
+             [1, 0, 0],
+             [0, 1, 0],
+             [0, 0, 1],
+
+             [1, 0, 0],
+             [0, 1, 0],
+             [0, 0, 1]]
+
+# A binary version of the y with respect to the class 2
 y_bin = [0, 0, 1,  0, 0, 1,  0, 0, 1]
 
 # An index from which the test part of the dataset starts
@@ -59,3 +81,29 @@ def test_fit_predict__fit_and_predict_with_all_at_on_multiclass_dataset__correct
     predictions = logistic_regression.predict(X)
 
     assert_array_equal(predictions, y)
+
+
+# This test imitates calculating cost and its gradient for binary classification problems or multiclass ones when
+# all_at_once is set to False and coefficients for each binary subproblems are being optimized separately
+def test_cost__binary_y_and_1D_coefs_passed__analytical_cost_gradient_equal_to_numerical_one():
+    y_np = to_numpy(y_bin)
+    coefs = np.ones(X_scaled.shape[1])
+
+    logistic_regression = LogisticRegression(regularization_param=1)
+    analytical_gradient = logistic_regression._cost_gradient(coefs, X_scaled, y_np)
+    numerical_gradient = gradient(coefs, logistic_regression._cost, (X_scaled, y_np))
+
+    assert_allclose(analytical_gradient, numerical_gradient)
+
+
+# This test imitates calculating cost and its gradient for multiclass problems when all_at_once is set to True and
+# coefficients for each binary subproblems are optimized all at once as a matrix
+def test_cost__one_hot_y_and_2D_coefs_passed__analytical_cost_gradient_equal_to_numerical_one():
+    y_np = to_numpy(y_one_hot)
+    coefs = np.ones((X_scaled.shape[1], np.shape(y_np)[1]))
+
+    logistic_regression = LogisticRegression(regularization_param=1)
+    analytical_gradient = logistic_regression._cost_gradient(coefs, X_scaled, y_np)
+    numerical_gradient = gradient(coefs, logistic_regression._cost, (X_scaled, y_np))
+
+    assert_allclose(analytical_gradient, numerical_gradient)
--- mymllib/regression/_tests/_linear_regression_test.py
+++ mymllib/regression/_tests/_linear_regression_test.py
@@ -1,6 +1,10 @@
 """Tests for the '_linear_regression' module."""
+import numpy as np
 from numpy.testing import assert_allclose
 from mymllib.regression import LinearRegression
+from mymllib.tools import gradient
+from mymllib.preprocessing import to_numpy
+
 
 # A simple dataset for tests in which all samples lie precisely on a straight line to make it perfect for linear
 # regression
@@ -21,3 +25,14 @@ def test_fit_predict__fit_and_predict_on_dataset__correct_predictions_returned()
     predictions = linear_regression.predict(X)
 
     assert_allclose(predictions, y)
+
+
+def test_cost__analytical_cost_gradient_equal_to_numerical_one():
+    X_np = to_numpy(X)
+    coefs = np.ones(X_np.shape[1])
+
+    linear_regression = LinearRegression(regularization_param=1)
+    analytical_gradient = linear_regression._cost_gradient(coefs, X_np, y)
+    numerical_gradient = gradient(coefs, linear_regression._cost, (X_np, y))
+
+    assert_allclose(analytical_gradient, numerical_gradient)
