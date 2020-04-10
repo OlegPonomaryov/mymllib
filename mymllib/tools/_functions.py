Refactor 'tools' package, add tests and fix a bug in the gradient() function

mymllib/tools/_functions.py
@@ -10,11 +10,14 @@ def gradient(x, func, args=(), h=1E-5):
     :param h: A change in x used to calculate the gradient
     :return: Approximate gradient of the function
     """
-    gradient = np.empty_like(x)
+    # It is necessary for gradient, a and b to be of float type, because if they inherit int type from x, assigning
+    # float values to their elements will result in 'truncating' this values (e.g. after running a[0] = 4.9999 value of
+    # a[0] will be equal to 4)
+    gradient = np.empty_like(x, dtype=float)
     for index in np.ndindex(x.shape):
-        a = x.copy()
+        a = x.astype(float)
+        b = x.astype(float)
         a[index] += h
-        b = x.copy()
         b[index] -= h
         gradient[index] = (func(a, *args) - func(b, *args)) / (2 * h)
     return gradient