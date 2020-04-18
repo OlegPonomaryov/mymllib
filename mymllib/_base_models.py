2691b11846d25a5e61d307f0550794d806cc8b90
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2020-04-18 16:29:15 +0300

    Move some functions from BaseRegression to BaseSupervisedModel

--- mymllib/_base_models.py
+++ mymllib/_base_models.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
+import numpy as np
 from mymllib.preprocessing import to_numpy
+from mymllib.optimization import LBFGSB
 
 
 class BaseModel(ABC):
@@ -7,25 +9,43 @@ class BaseModel(ABC):
 
 
 class BaseSupervisedModel(BaseModel):
-    """Base class for supervised models."""
+    """Base class for supervised models.
 
-    @abstractmethod
+    :param regularization_param: L2 regularization parameter (must be >= 0, when set exactly to 0 no regularization is
+        used)
+    :param optimizer: An optimizer to use for minimizing a cost function
+    """
+
+    def __init__(self, regularization_param, optimizer=LBFGSB()):
+        assert regularization_param >= 0, "Regularization parameter must be >= 0, but was negative."
+        self._regularization_param = regularization_param
+        self._params = None
+        self._optimizer = optimizer
+
     def fit(self, X, y):
         """Train the model.
 
         :param X: Features values
         :param y: Target values
         """
-        pass
+        self._params = self._optimize_params(X, y, np.zeros(X.shape[1]))
 
-    @abstractmethod
     def predict(self, X):
         """Predict target values.
 
         :param X: Features values
         :return: Target values
         """
-        pass
+        if X.ndim != 2:
+            raise ValueError("Features values (X) should be a two-dimensional array")
+
+        if X.shape[1] != self._params.shape[0]:
+            raise ValueError(f"Expected {self._params.shape[0]} features, but {X.shape[1]} received")
+
+        return self._hypothesis(X, self._params)
+
+    def _optimize_params(self, X, y, initial_params):
+        return self._optimizer.minimize(self._cost, self._cost_gradient, initial_params, (X, y))
 
     @staticmethod
     def _check_data(X, y):
@@ -47,3 +67,15 @@ class BaseSupervisedModel(BaseModel):
             raise ValueError("Samples count in features values (X) and target values (y) don't match each other")
 
         return X, y
+
+    @abstractmethod
+    def _hypothesis(self, X, params):
+        pass
+
+    @abstractmethod
+    def _cost(self, params, X, y):
+        pass
+
+    @abstractmethod
+    def _cost_gradient(self, params, X, y):
+        pass
--- mymllib/regression/_base_regression.py
+++ mymllib/regression/_base_regression.py
@@ -1,55 +1,9 @@
-import numpy as np
-from mymllib.optimization import LBFGSB
+from abc import ABC
 from mymllib._base_models import BaseSupervisedModel
-from abc import abstractmethod
 
 
-class BaseRegression(BaseSupervisedModel):
-    """Base class for regressions.
-
-    :param regularization_param: L2 regularization parameter (must be >= 0, when set exactly to 0 no regularization is
-        used)
-    :param optimizer: An optimizer to use for minimizing a cost function
-    """
-
-    def __init__(self, regularization_param, optimizer=LBFGSB()):
-        assert regularization_param >= 0, "Regularization parameter must be >= 0, but was negative."
-        self._regularization_param = regularization_param
-        self._params = None
-        self._optimizer = optimizer
-
-    def fit(self, X, y):
-        """Train the model.
-
-        :param X: Features values
-        :param y: Target values
-        """
-        self._params = self._optimize_params(X, y, np.zeros(X.shape[1]))
-
-    def predict(self, X):
-        """Predict target values.
-
-        :param X: Features values
-        :return: Target values
-        """
-        if X.ndim != 2:
-            raise ValueError("Features values (X) should be a two-dimensional array")
-
-        if X.shape[1] != self._params.shape[0]:
-            raise ValueError(f"Expected {self._params.shape[0]} features, but {X.shape[1]} received")
-
-        return self._hypothesis(X, self._params)
-
-    def _optimize_params(self, X, y, initial_params):
-        return self._optimizer.minimize(self._cost, self._cost_gradient, initial_params, (X, y))
-
-    @abstractmethod
-    def _hypothesis(self, X, params):
-        pass
-
-    @abstractmethod
-    def _cost(self, params, X, y):
-        pass
+class BaseRegression(BaseSupervisedModel, ABC):
+    """Base class for regressions."""
 
     def _cost_gradient(self, params, X, y):
         # Intercept should not be regularized, so it is set to 0 in a copy of the parameters vector
