e3f8600e989aebdfe8af5f6d15d673473e436470
Oleg Ponomaryov <ponomaryov.oleg@gmail.com>
2020-05-23 17:20:05 +0300

    Remove all_at_once option from logistic regression

--- mymllib/regression/_logistic_regression.py
+++ mymllib/regression/_logistic_regression.py
@@ -13,15 +13,11 @@ class LogisticRegression(BaseRegression):
     :param optimizer: An optimizer to use for minimizing a cost function
     :param predict_probabilities: Whether to return probabilities of samples to belong to each of the classes (ordered
         by their labels) instead of chosen classes
-    :param all_at_once: Whether to optimize all binary classification subproblems of a multiclass problem as a single
-        optimization target
     """
 
-    def __init__(self, regularization_param=0, optimizer=LBFGSB(), predict_probabilities=False,
-                 all_at_once=False):
+    def __init__(self, regularization_param=0, optimizer=LBFGSB(), predict_probabilities=False):
         super().__init__(regularization_param=regularization_param, optimizer=optimizer)
         self._predict_probabilities = predict_probabilities
-        self._all_at_once = all_at_once
         self._labels = None
 
     def fit(self, X, y):
@@ -33,13 +29,7 @@ class LogisticRegression(BaseRegression):
         X, y = self._check_fit_data(X, y)
         X = add_intercept(X)
         self._labels, Y = one_hot(y)
-
-        if self._all_at_once:
-            initial_params = np.zeros((X.shape[1], Y.shape[1]))
-            params = self._optimize_params(X, Y, unroll((initial_params,)))
-            self._params = undo_unroll(params, (initial_params.shape,))[0]
-        else:
-            self._params = np.apply_along_axis(lambda y_bin: self._optimize_params(X, y_bin, np.zeros(X.shape[1])), 0, Y)
+        self._params = np.apply_along_axis(lambda y_bin: self._optimize_params(X, y_bin, np.zeros(X.shape[1])), 0, Y)
 
     def predict(self, X):
         """Predict target values.
